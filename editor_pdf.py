4480750ff5d82bb479cf91d02bf71d01b8bf9040
Carlos Alexandre <alexandrec191@gmail.com>
2026-07-29 12:53:18 -0300

    Testes nas coordenadas da etiqueta

--- editor_pdf.py
+++ editor_pdf.py
@@ -1,6 +1,5 @@
 import pymupdf as fitz
 
-
 def adicionar_lote(pdf_entrada, pdf_saida, numero_lote):
 
     doc = fitz.open(pdf_entrada)
@@ -11,14 +10,26 @@ def adicionar_lote(pdf_entrada, pdf_saida, numero_lote):
         altura = pagina.rect.height
 
         print(f"Largura: {largura}")
-        print(f"Altura : {altura}")
-        
+        print(f"Altura: {altura}")
 
-        # Marca os quatro cantos
-        pagina.insert_text((10, 20), "A", fontsize=20, color=(1, 0, 0))
-        pagina.insert_text((largura - 20, 20), "B", fontsize=20, color=(0, 1, 0))
-        pagina.insert_text((10, altura - 20), "C", fontsize=20, color=(0, 0, 1))
-        pagina.insert_text((largura - 20, altura - 20), "D", fontsize=20, color=(1, 0, 1))
+        # TESTE DE RÉGUA
+        for x in range(0, int(largura), 50):
+            pagina.insert_text(
+                (x, 50),
+                f"X{x}",
+                fontsize=12,
+                color=(1, 0, 0)
+            )
+
+        for y in range(0, int(altura), 50):
+            pagina.insert_text(
+                (20, y),
+                f"Y{y}",
+                fontsize=12,
+                color=(0, 0, 1)
+            )
 
     doc.save(pdf_saida)
     doc.close()
+
+    print("PDF criado com sucesso!")
