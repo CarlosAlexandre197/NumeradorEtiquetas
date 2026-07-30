e4c638808c18da18874f4b15ac4bd3210b17595d
Carlos Alexandre <alexandrec191@gmail.com>
2026-07-30 12:19:25 -0300

    Correções no executável

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import sys
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QIcon
 from janela_principal import JanelaPrincipal
+
 
 def main():
     app = QApplication(sys.argv)
+
+    # Ícone da aplicação
+    app.setWindowIcon(QIcon("icone.ico.ico"))
 
     janela = JanelaPrincipal()
     janela.show()
